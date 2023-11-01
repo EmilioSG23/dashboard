c5d4970eeb220dde81bb1355771b21dbba1ec079
Cliente CONTROL-PC <CltControl@GGIECLUGWRK161.espol.edu.ec>
2023-11-01 10:24:39 -0500

    Nuevo grafico

--- public/javascript/load_data.js
+++ public/javascript/load_data.js
@@ -26,11 +26,40 @@ let cargarPrecipitacion = () => {
     document.getElementById("precipitacionMaxValue").textContent = `Max ${precMax} [mm]`;
 }
 
+let cargarUV = () => {
+    let datos = [];
+    for (let i=0; i<uvArr.length;i++)
+        if (tiempoArr[i].includes(fechaActual()))
+            datos.push(uvArr[i]);
+    let uvMax = Math.max(...datos);
+    let uvMin = Math.min(...datos);
+    let uvSum = datos.reduce((a,b) => a + b, 0);
+    let uvProm = (uvSum/datos.length) || 0;
+
+    document.getElementById("uvMinValue").textContent = `Min ${uvMin} [--]`;
+    document.getElementById("uvPromValue").textContent = `Prom ${Math.round(uvProm * 100) / 100 } [--]`;
+    document.getElementById("uvMaxValue").textContent = `Max ${uvMax} [--]`;
+}
+
+let cargarTemperatura = () => {
+    let datos = [];
+    for (let i=0; i<uvArr.length;i++)
+        if (tiempoArr[i].includes(fechaActual()))
+            datos.push(temperaturaArr[i]);
+    let temperaturaMax = Math.max(...datos);
+    let temperaturaMin = Math.min(...datos);
+    let temperaturaSum = datos.reduce((a,b) => a + b, 0);
+    let temperaturaProm = (temperaturaSum/datos.length) || 0;
+
+    document.getElementById("temperaturaMinValue").textContent = `Min ${temperaturaMin} [°C]`;
+    document.getElementById("temperaturaPromValue").textContent = `Prom ${Math.round(temperaturaProm * 100) / 100 } [°C]`;
+    document.getElementById("temperaturaMaxValue").textContent = `Max ${temperaturaMax} [°C]`;
+}
+
 let cargarOpenMeteo = () => {
     //TEMPERATURA
     //URL que responde con la respuesta a cargar
-    let URL = 'https://api.open-meteo.com/v1/forecast?latitude=-2.1962&longitude=-79.8862&hourly=temperature_2m&timezone=auto'; 
-  
+    let URL = 'https://api.open-meteo.com/v1/forecast?latitude=-2.1962&longitude=-79.8862&hourly=precipitation_probability&timezone=auto';
     fetch( URL )
       .then(responseText => responseText.json())
       .then(responseJSON => {
@@ -44,7 +73,7 @@ let cargarOpenMeteo = () => {
         let labels = responseJSON.hourly.time;
 
         //Etiquetas de los datos
-        let data = responseJSON.hourly.temperature_2m;
+        let data = responseJSON.hourly.precipitation_probability;
 
         //Objeto de configuración del gráfico
         let config = {
@@ -53,7 +82,7 @@ let cargarOpenMeteo = () => {
                 labels: labels, 
                 datasets: [
                 {
-                    label: 'Temperature [2m]',
+                    label: 'Probabilidad de Precipitación',
                     data: data, 
                 }
                 ]
@@ -104,9 +133,51 @@ let cargarOpenMeteo = () => {
 
     })
     .catch(console.error);
+
+    //TEMPERATURA
+    //URL que responde con la respuesta a cargar
+    URL = 'https://api.open-meteo.com/v1/forecast?latitude=-2.1962&longitude=-79.8862&hourly=temperature_2m&timezone=auto'; 
+  
+    fetch( URL )
+      .then(responseText => responseText.json())
+      .then(responseJSON => {
+        
+        //Respuesta en formato JSON
+
+        //Referencia al elemento con el identificador plot
+        let plotRef = document.getElementById('plot3');
+
+        //Etiquetas del gráfico
+        let labels = responseJSON.hourly.time;
+
+        //Etiquetas de los datos
+        let data = responseJSON.hourly.temperature_2m;
+
+        //Objeto de configuración del gráfico
+        let config = {
+            type: 'line',
+            data: {
+                labels: labels, 
+                datasets: [
+                {
+                    label: 'Temperature [2m]',
+                    data: data, 
+                }
+                ]
+            }
+        };
+
+        //Objeto con la instanciación del gráfico
+        let chart3  = new Chart(plotRef, config);
+
+    })
+    .catch(console.error);
+
 }
 
   cargarOpenMeteo();
 
   cargarFechaActual();
-  cargarPrecipitacion()
+  cargarPrecipitacion();
+  cargarUV();
+  cargarTemperatura()
