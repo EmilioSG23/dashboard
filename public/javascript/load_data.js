c086919383d4f49274c947e2f60ca2c419c1fb09
Cliente CONTROL-PC <CltControl@GGIECLUGWRK161.espol.edu.ec>
2023-11-13 10:01:11 -0500

    El cors

--- public/javascript/load_data.js
+++ public/javascript/load_data.js
@@ -260,7 +260,23 @@ let parseXML = (responseText) => {
 
   }
   
+  let loadExternalTable = async () => {
+    
+    let proxyURL = 'https://cors-anywhere.herokuapp.com/'
+    let endpoint = proxyURL + 'https://www.gestionderiesgos.gob.ec/monitoreo-de-inundaciones/'
+    //Requerimiento asíncrono
+    //let endpoint = 'https://www.gestionderiesgos.gob.ec/monitoreo-de-inundaciones/';
+    let response = await fetch(endpoint);
+    let responseText = await response.text();
+    //LOCAL STORAGE EMILIO!!!!!!!!
+    const parser = new DOMParser();
+    const xml = parser.parseFromString(responseText, "text/html");
 
+    let elementoXML = xml.querySelector("#postcontent table");
+    let elementoDOM = document.getElementById("monitoreo");
+
+    elementoDOM.innerHTML = elementoXML.outerHTML;
+   }
 
 /* --- INVOCACIÓN DE FUNCIONES --- */
 /*document.getElementById("btn_precipitacion").onclick = () => {cargarGraficoPrecipitacion();}
@@ -268,6 +284,7 @@ document.getElementById("btn_uv").onclick = () => {cargarGraficoUV();}
 document.getElementById("btn_temperatura").onclick = () => {cargarGraficoTemperatura();}*/
 
 loadForecastByCity();
+loadExternalTable();
 cargarFechaActual();
 cargarGraficoPrecipitacion();
 cargarGraficoUV();
